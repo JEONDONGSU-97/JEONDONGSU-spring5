b6527104bff033af537ffc61db4c655831d735f9
User <User@DESKTOP-HOHL5IM>
2021-06-01 14:37:12 +0900

    5교시 커밋

---
+++ src/main/webapp/resources/test/json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e127c003f067714f7b6a5",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Geraldine Avery"
+    },
+    {
+      "_id": "609e127cb7f3f0fe18ebe849",
+      "age": 37,
+      "eyeColor": "brown",
+      "name": "Esperanza Hoover"
+    },
+    {
+      "_id": "609e127ce858dee9ac53fe78",
+      "age": 38,
+      "eyeColor": "blue",
+      "name": "Tammy Murphy"
+    },
+    {
+      "_id": "609e127c47ca3f999ca8c217",
+      "age": 22,
+      "eyeColor": "brown",
+      "name": "Petra Wood"
+    },
+    {
+      "_id": "609e127c0492b96dcbfd7e20",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Ashley Jones"
+    },
+    {
+      "_id": "609e127c71d780f8cfff60d5",
+      "age": 23,
+      "eyeColor": "brown",
+      "name": "Kenya Campos"
+    },
+    {
+      "_id": "609e127cd1261bf2e49bcfb3",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Alfreda Vang"
+    },
+    {
+      "_id": "609e127c9a2261ecc4fe2865",
+      "age": 33,
+      "eyeColor": "green",
+      "name": "Cathy Reynolds"
+    },
+    {
+      "_id": "609e127c03d4ff34f6d38762",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Kristie Nunez"
+    },
+    {
+      "_id": "609e127cc0bbbf2a0f60ab57",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Elisa Conrad"
+    },
+    {
+      "_id": "609e127c0527e967b88ccee0",
+      "age": 32,
+      "eyeColor": "green",
+      "name": "Lavonne Kennedy"
+    },
+    {
+      "_id": "609e127ccfd71da20f4960f1",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Fay Bond"
+    },
+    {
+      "_id": "609e127c12039c17069a3f20",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Kate Melton"
+    },
+    {
+      "_id": "609e127cb520b135a41c7cd3",
+      "age": 28,
+      "eyeColor": "blue",
+      "name": "Harris Barr"
+    },
+    {
+      "_id": "609e127c90abb7b77adbf0d3",
+      "age": 30,
+      "eyeColor": "green",
+      "name": "Maxine May"
+    },
+    {
+      "_id": "609e127c1603e7b0c55ccbd3",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Angelita Cash"
+    },
+    {
+      "_id": "609e127cb8492245b1041d57",
+      "age": 34,
+      "eyeColor": "brown",
+      "name": "Shari Burgess"
+    },
+    {
+      "_id": "609e127cd995d56d59cfe2ba",
+      "age": 21,
+      "eyeColor": "brown",
+      "name": "May Gill"
+    },
+    {
+      "_id": "609e127cb03a31bdc95d1400",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "Simon Holder"
+    },
+    {
+      "_id": "609e127cef28d1ebcf5d2e61",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Concepcion Mccall"
+    }
+  ]
