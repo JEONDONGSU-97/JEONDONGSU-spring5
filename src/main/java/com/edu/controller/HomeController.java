489ef7ed2a9a5999096783aa0d413043bed8fae1
User <User@DESKTOP-HOHL5IM>
2021-06-01 16:39:12 +0900

    7class commit

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	//스프링빈(클래스)에서는 로거로 디버그를 한다. = 로거객체를 만든다.
 	// 로그중 slf4j(Spring Log For Java)
-    //private Logger logger = Logger.
-	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    //private Logger logger = Logger. 
+	//private static final Logger logger = LoggerFactory.getLogger(HomeController.class); 
 	
 	/**
 	 * 사용자요청(웹브라우저)을 받아서=@RequestMapping인터페이스를 사용해서 메서드명을 스프링이 구현한다.
